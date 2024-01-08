bf4129ec5656f84366484491dcc1af2988cab080
AmeyaKhanzode <ameyakhanzode2022@gmail.com>
2024-01-08 20:19:07 +0530

    Code enhancing commit

--- Password.py
+++ Password.py
@@ -12,8 +12,10 @@ class Password:
         while i<=self.size:
             index1 = random.randint(0,25)
             index2 = random.randint(0,25)
-            random_characters = [chars[index1],chars[index2].upper(),str(random.randint(0,10))]
-            randomnum = random.randint(0,2)
+            index3 = random.randint(0,6)
+            special_chars = ['!','@','#','$','%','&','*']
+            random_characters = [chars[index1],chars[index2].upper(),str(random.randint(0,10)),special_chars[index3]]
+            randomnum = random.randint(0,3)
             password += random_characters[randomnum]
             i = i+1
         print(f"Random Password is: {password}")
