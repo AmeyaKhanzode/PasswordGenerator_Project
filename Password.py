98d156767546a3698d9ca94a346cf11511fa63bc
AmeyaKhanzode <ameyakhanzode2022@gmail.com>
2024-01-08 19:50:46 +0530

    Adding py file

---
+++ Password.py
@@ -0,0 +1,27 @@
+class Password:
+    def __init__(self,size):
+        self.size = size
+    
+    def generate(self):
+        import random
+        import string
+        chars = list(string.ascii_lowercase)
+
+        password = ""
+        i = 0
+        while i<=self.size:
+            index1 = random.randint(0,25)
+            index2 = random.randint(0,25)
+            random_characters = [chars[index1],chars[index2].upper(),str(random.randint(0,10))]
+            randomnum = random.randint(0,2)
+            password += random_characters[randomnum]
+            i = i+1
+        print(f"Random Password is: {password}")
+
+password_size = int(input("Enter the length of the password that you want: "))
+p1 = Password(password_size)
+p1.generate()
+
+
+
+
